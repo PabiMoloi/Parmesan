c104d8cd8a3faf6f6e7a00387c5430d830a08f7d
PabiMoloi <moloipaballo@gmail.com>
2018-05-04 08:40:59 +0200

    Project Model
    - Added a meal model for the project.

---
+++ Parmesan/Meal.swift
@@ -0,0 +1,28 @@
+//
+//  Meal.swift
+//  Parmesan
+//
+//  Created by DVT on 2018/05/04.
+//  Copyright © 2018 DVT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Meal {
+    
+    var title: String
+    var photo: UIImage?
+    var category: String?
+    
+    init? (title: String, photo:UIImage?, category: String?){
+        
+        if title.isEmpty{
+            return nil
+        }
+        
+        self.title = title
+        self.photo = photo
+        self.category = category
+    }
+}
