149b760b8fd75bf1287b788abba88575b04d8c5d
PabiMoloi <moloipaballo@gmail.com>
2018-05-14 11:44:48 +0200

    Repository Addition
    - Added repository and meal list viewmodel

---
+++ Parmesan/Repository/MealRepositoryImpl.swift
@@ -0,0 +1,28 @@
+//
+//  MealRepositoryImpl.swift
+//  Parmesan
+//
+//  Created by DVT on 2018/05/14.
+//  Copyright © 2018 DVT. All rights reserved.
+//
+
+import Foundation
+
+struct MealRepositoryImpl : MealRepository {
+    
+    var meals: [Meal] = []
+    var mealRecipe: Meal? = nil
+    
+    func getMealList() -> [Meal] {
+        return meals
+    }
+    
+    func getMealRecipe(mealId: String) -> Meal? {
+        for mealRecipe in meals {
+            if mealRecipe.mealId = mealId {
+                return mealRecipe
+            }
+        }
+        return nil
+    }
+}
